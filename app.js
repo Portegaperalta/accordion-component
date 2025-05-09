9b642a208b45c8f43032eea4ff79f5075ca4511b
pablothecoder <ortegaperaltap@gmail.com>
2025-05-09 18:59:28 -0400

    html css and js files updated

---
+++ app.js
@@ -0,0 +1,13 @@
+const answers = {
+  answer1: "Yes, FrontendPro prepares you to develop the skills neccesary to land your first job.",
+  answer2: "FrontendPro is designed to accommodate developers of all skill levels. Our challenges range from beginner to advanced, so there's something for everyone.",
+  answer3: "We add new challenges every week for you to constantly improve.",
+  answer4: "You can expect challenges of all types, from beginner, to intermediate, to advanced.",
+  answer5: "No, it is not practical for you to use them, you should use pure hardcode code.",
+}
+
+const questions = document.querySelectorAll('.question')
+const answerBox = document.querySelectorAll('.question-bottom p')
+const moreBtns = document.querySelectorAll('#moreBtn')
+const lessBtns = document.querySelectorAll('#lessBtn')
+
